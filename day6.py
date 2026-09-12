d695544e2bbf7dede27c4a3d96442d55dbd15c51
hardy-sys <19121267531@163.com>
2026-09-12 21:51:09 +0800

    add main.py

---
+++ day6.py
@@ -0,0 +1,19 @@
+num1 = 10
+num2 = 3
+print(num1)
+print(num2)
+print("==========================")
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+sum = num1 + num2
+print(sum)
+print("===========================")
+num3 = num4 = 100
+print(num3)
+print(num4)
+print("===========================")
+num5,num6 = 200,300
+print(num5)
+print(num6)
---
+++ day7.py
@@ -0,0 +1,14 @@
+var1 = 10
+var2 = 20
+temp = var1
+var1 = var2
+var2 = temp
+print(var1)
+print(var2)
+print("==================================")
+var3 = 100
+var4 = 200
+var3, var4 = var4, var3
+print(var3)
+print(var4)
+print("==================================")
