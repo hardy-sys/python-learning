99168d95c7e9d110e65f6c0cf34a13f2f4070e82
hardy-sys <19121267531@163.com>
2026-09-14 18:29:16 +0800

    add day10.py

---
+++ day10.py
@@ -0,0 +1,19 @@
+num1 = 10
+num2 = 10
+print(f"10 == 10结果是：{num1 == num2}")
+
+num1 = 10
+num2 = 10
+print(f"10 !=15的结果是{num1 != num2}")
+
+name1 = ("hello")
+name2 = ("world")
+print(f"hello = world 结果{name1 == name2}")
+
+num1 = 10
+num2 = 10
+print(f"10 > 5的结果是{num1 >= num2}")
+print(f"10 < 10的结果是{num1 <= num2}")
+
+
+
