683d79924882434b227ea95844596b1facaf33de
hardy-sys <19121267531@163.com>
2026-09-14 12:58:46 +0800

    add day9.py

---
+++ day9.py
@@ -0,0 +1,13 @@
+num = 666
+print(int(num))
+print(type(int(num)))
+
+num = "5.21"
+print(float(num))
+print(type(float(num)))
+print(float(5))
+
+num = 13.14
+print(str(num))
+print(type(str(num)))
+
